f237c83ce76bf2d3591daf508df6df974d141ff1
Andres Ramirez <andresaaramirez@aol.com>
2020-08-19 12:24:37 -0400

    installed package lock

--- Develop/index.js
+++ Develop/index.js
@@ -19,11 +19,6 @@ function promptUser() {
       },
       {
         type: "input",
-        name: "Table of Contents",
-        message: "What is the table of contents of your readme?"
-      },
-      {
-        type: "input",
         name: "Installation",
         message: "what are the instructions for instalation for your readme?"
       },
@@ -63,7 +58,7 @@ function promptUser() {
   
 // function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile('readme.txt', data, fileName, (err) => {
+  fs.writeFile('readme.md', data, fileName, (err) => {
     if (err) throw err;
     console.log('the readme has been generated')
   });   
@@ -77,14 +72,14 @@ async function init() {
 
     const readMe = generateMarkdown(responses);
 
-    await writeFileAsync("readme.txt", readMe);
+    await writeFileAsync("readme.md", readMe);
 
-    console.log("Successfully wrote to readme.txt");
+    console.log("Successfully wrote to readme.md");
 
   } catch(err) {
     console.log(err);
   }
-
+  //writeToFile();
 }
 
 // function call to initialize program
@@ -92,21 +87,3 @@ init();
 
 
 
-
-/*
-GIVEN a command-line application that accepts user input
-WHEN I am prompted for information about my application repository
-THEN a quality, professional README.md is generated with the title of your project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-WHEN I enter my project title
-THEN this is displayed as the title of the README
-WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-WHEN I choose a license for my application from a list of options
-THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-WHEN I enter my GitHub username
-THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-WHEN I enter my email address
-THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-WHEN I click on the links in the Table of Contents
-THEN I am taken to the corresponding section of the README
-*/
